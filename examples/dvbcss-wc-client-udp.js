10e5faf502dfcfb3c01053ab4e15849d5f8ac9ea
ramdhany <nirish777@gmail.com>
2016-10-25 00:12:24 +0100

    minor changes - updated IP address of server endpoints

--- examples/dvbcss-wc-client-udp.js
+++ examples/dvbcss-wc-client-udp.js
@@ -8,7 +8,7 @@ var wallClock = new clocks.CorrelatedClock(sysClock);
 var udpSocket = dgram.createSocket({type:'udp4', reuseAddr:true});
 
 var protocolOptions = {
-    dest: { address:"127.0.0.1", port:6677 }
+    dest: { address:"192.168.0.11", port:6677 }
 };
 
 udpSocket.on('listening', function() {
